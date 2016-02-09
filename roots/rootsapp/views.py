31b1d89c253e851aa428c6c697287712b62fb493
Choukri <chousoulei@gmail.com>
2016-02-09 19:32:50 +0100

    Suppression de rootsapp

--- roots/rootsapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-
-def index(request):
-	#template_name = 'rootsapp/index.html'
-    return render(request, 'rootsapp/index.html')
-
